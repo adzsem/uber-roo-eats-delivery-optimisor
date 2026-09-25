595e9f8db332f129afbb1728cbe9c7d5a1192b06
adzsem <adamsemmakie@gmail.com>
2026-09-25 19:27:38 +0100

    Add rider configuration with 19 mph bike cap

---
+++ app/src/main/java/uk/jev/rider/config/RiderConfig.kt
@@ -0,0 +1,13 @@
+package uk.jev.rider.config
+
+object RiderConfig {
+    const val BIKE_MAX_SPEED_MPH = 19.0
+
+    const val MINIMUM_POUNDS_PER_MILE = 1.20
+    const val PREFERRED_POUNDS_PER_MILE = 1.80
+
+    const val MINIMUM_POUNDS_PER_HOUR = 12.0
+    const val PREFERRED_POUNDS_PER_HOUR = 18.0
+
+    const val RESERVE_RANGE_MILES = 4.0
+}
